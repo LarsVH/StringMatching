f31f7794880c8463a08629f49b0092c6b9c316c2
Lars Van Holsbeeke <lavholsb@vub.ac.be>
2016-05-30 11:22:57 +0200

    added dummystrings

---
+++ src/com/lars/DummyStrings.java
@@ -0,0 +1,35 @@
+package com.lars;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lars on 29.05.16.
+ */
+public class DummyStrings {
+
+    private ArrayList<String> dummy = new ArrayList<>();
+
+    public ArrayList<String> getDummy(int nr){
+
+        switch(nr){
+            case 1:
+                dummy.add("Location");
+                dummy.add("Place");
+                dummy.add("Kitchen");
+                dummy.add("PersonInRoom");
+                dummy.add("PersonInLocation");
+                dummy.add("PersonAtPlace");
+                dummy.add("SandraInLocation");
+                dummy.add("SandraInKitchen");
+                dummy.add("SandraInLivingRoom");
+                dummy.add("CisaMeeting");
+                dummy.add("VinceSleeps");
+                dummy.add("SInBedRoom");
+                break;
+            case 2:
+                break;
+        }
+        return dummy;
+    }
+
+}
